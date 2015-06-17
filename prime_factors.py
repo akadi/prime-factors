e8996d83dab3f575b66f9997b09992620db1aa2d
abdelhalim kadi <akadi@sewan.fr>
2015-06-17 15:19:59 +0200

    Somes improvement

--- prime_factors.py
+++ prime_factors.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
-# Author(s): Abdelhalim Kadi <kadi.halim@gmail.com>
-#Compute the prime factors of a given natural number.
+# Author Abdelhalim Kadi <kadi.halim@gmail.com>
+# Compute the prime factors of a given natural number.
 
 def get_prime_factors(number):
-    u"""
+    """
     Return prime factor list for a given number
         number - an integer number
         Example: get_prime_factors(8) --> [2, 2, 2].
@@ -11,12 +11,12 @@ def get_prime_factors(number):
     if number == 1:
         return []
 
-    #We have to begin with 2 instead of 1 or 0
-    #to avoid the calls infinite or the division by 0
+    # We have to begin with 2 instead of 1 or 0
+    # to avoid the calls infinite or the division by 0
     for i in xrange(2, number):
-        #Get remainder and quotient
+        # Get remainder and quotient
         rd, qt = divmod(number, i)
-        if qt == 0:
+        if not qt: # if equal to zero
             return [i] + get_prime_factors(rd)
 
     return [number]
